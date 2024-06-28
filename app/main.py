195b6532363aa33dc944e6b2dd6677008028e2ed
valsora <valsora9@gmail.com>
2024-06-28 15:47:22 +0300

    fifth

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routing.vacancies import router as vacancies
-
+from app.routing.vacancies import router as vacancies
 
 app = FastAPI()
+
 app.include_router(vacancies)
