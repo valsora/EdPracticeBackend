a55c4dfbc7ecbd1036ffa5d052edd1cbc4a54054
valsora <valsora9@gmail.com>
2024-07-04 17:37:05 +0500

    added cors because POST didnt work

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routing.vacancies import router as vacancies_router
 from app.storage.db import Base, engine
 
@@ -7,6 +8,14 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(vacancies_router, prefix='/vacancies', tags=['Vacancies'])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 
 @app.get('/')
